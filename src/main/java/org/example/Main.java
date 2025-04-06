6c7bf0e00da33cb748480e4a503c114082c24ac9
Gracela Peto <petogracela@gmail.com>
2025-04-06 15:58:48 +0200

    added HibernateUtils

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,13 @@
 package org.example;
+
+import org.example.config.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
-
+        HibernateUtils hibernateUtils=new HibernateUtils();
+        SessionFactory sessionFactory= hibernateUtils.getSessionFactory();
+        Session session=sessionFactory.openSession();
     }
 }
---
+++ src/main/java/org/example/config/HibernateUtils.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+import org.example.entities.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+    private SessionFactory sessionFactory;
+    public  SessionFactory getSessionFactory(){
+        if (sessionFactory==null){
+            Configuration configuration=new Configuration();//perdorimi i single tonit ne kte bllok  lidhja me datbasen
+            configuration.addAnnotatedClass(Appointment.class);
+            configuration.addAnnotatedClass(Doctor.class);
+            configuration.addAnnotatedClass(Patient.class);
+            configuration.addAnnotatedClass(Paymant.class);
+            configuration.addAnnotatedClass(Presciption.class);
+            sessionFactory =configuration.buildSessionFactory();
+
+        }
+        return this.sessionFactory;
+    }
+}
